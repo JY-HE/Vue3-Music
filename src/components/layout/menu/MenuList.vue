8eeae74da5a76e5308d468df5a861c639bd470be
何嘉泳 <893404614@qq.com>
2023-02-19 13:44:58 +0800

    feat：删除useMenu.ts

--- src/components/layout/menu/MenuList.vue
+++ src/components/layout/menu/MenuList.vue
@@ -6,7 +6,7 @@
                 v-for="(item, index) in menu.menus"
                 :key="item.name"
                 class="h-9 text-[1.125rem] flex items-center pl-4 menu"
-                :class="item.key === currentKey ? 'active' : ''"
+                :class="item.key === state.currentKey ? 'active' : ''"
                 @click="changeMenu(item)"
             >
                 <IconPark :icon="item.icon" size="20" :theme="item.theme" />
@@ -18,24 +18,99 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
-import { useMenu } from '@/hooks';
-import type { IMenu } from '@/models/menu';
+import type { IMenus, IMenu } from '@/models/menu';
+import {
+    Planet as iconPlanet,
+    Music as iconMusic,
+    VideoOne as iconVideoOne,
+    Fm as iconFm,
+    Like as iconLike,
+    Computer as iconComputer,
+    DownloadThree as iconDownloadThree,
+    PlayTwo as iconPlayTwo,
+} from '@icon-park/vue-next';
 
 const router = useRouter();
-const { menus, setCurrentKey, currentKey } = useMenu();
+
+const state = reactive({
+    currentKey: (router.currentRoute?.value?.meta?.menu as string) || '',
+});
+
+const menus = computed<IMenus[]>(() => {
+    return [
+        {
+            name: '在线音乐',
+            menus: [
+                {
+                    name: '推荐',
+                    key: 'discover',
+                    icon: iconPlanet,
+                    theme: 'outline',
+                },
+                {
+                    name: '音乐馆',
+                    key: 'music',
+                    icon: iconMusic,
+                    theme: 'outline',
+                },
+                {
+                    name: '视频',
+                    key: 'video',
+                    icon: iconVideoOne,
+                    theme: 'outline',
+                },
+                {
+                    name: '电台',
+                    key: 'dj',
+                    icon: iconFm,
+                    theme: 'outline',
+                },
+            ],
+        },
+        {
+            name: '我的音乐',
+            menus: [
+                {
+                    name: '我喜欢',
+                    key: 'love',
+                    icon: iconLike,
+                    theme: 'outline',
+                },
+                {
+                    name: '本地歌曲',
+                    key: 'local',
+                    icon: iconComputer,
+                    theme: 'outline',
+                },
+                {
+                    name: '下载歌曲',
+                    key: 'download',
+                    icon: iconDownloadThree,
+                    theme: 'outline',
+                },
+                {
+                    name: '最近播放',
+                    key: 'recently',
+                    icon: iconPlayTwo,
+                    theme: 'outline',
+                },
+            ],
+        },
+    ];
+});
 
 watch(
     () => router.currentRoute.value.meta,
     newValue => {
         console.log('Rd ~ file: MenuList.vue:41 ~ newValue', newValue);
-        setCurrentKey(newValue.menu as string);
+        state.currentKey = newValue.menu as string;
     },
     { immediate: true }
 );
 
 const changeMenu = async (menu: IMenu) => {
     console.log('Rd ~ file: MenuList.vue:38 ~ changeMenu ~ menu', menu);
-    setCurrentKey(menu.key);
+    state.currentKey = menu.key;
     await router.push({ name: menu.key || '/', replace: true, params: {} });
 };
 </script>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -9,4 +9,3 @@
  */
 
 export * from './useUser'
-export * from './useMenu'
